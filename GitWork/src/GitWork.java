378b0acdaf976e5d8adcb967d7f82470a4be3969
Anjum <Anjum@Anjum>
2017-01-03 23:16:09 +0530

    Initial check in of GitWork

---
+++ GitWork/src/GitWork.java
@@ -0,0 +1,11 @@
+
+public class GitWork {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is exmple for setting up git");
+
+	}
+
+}
